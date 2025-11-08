cd7a02d81cf1b034d9a20f3939e2e39ba614a8f8
Sai Shashwath AR <saishashwath2004@gmail.com>
2025-11-08 19:07:36 +0530

    added all the routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import {login_user, register_user,logout_user, refresh_access_token} from "../controllers/user.controllers.js"
+import {login_user, 
+        register_user,
+        logout_user, 
+        refresh_access_token, 
+        change_curr_password, 
+        get_curr_user, 
+        update_user_avatar, 
+        update_user_cover_image, 
+        get_user_channel_profile, 
+        get_watch_history} 
+        from "../controllers/user.controllers.js"
 
 import {upload} from "../middlewares/multer.middleware.js"
 import { verify_jwt } from "../middlewares/auth.middleware.js";
@@ -25,4 +35,12 @@ router.route("/login").post(login_user)
 router.route("/logout").post(verify_jwt,logout_user) // verify_jwt is the middleware whihc is injected before logout_user is executed
 
 router.route("/refresh_token").post(refresh_access_token)
+
+router.route("/change_password").post(verify_jwt,change_curr_password)
+router.route("/current_user").get(verify_jwt,get_curr_user)
+router.route("/update_account").patch(verify_jwt,get_curr_user) // write patch so tht not all details get overwritten, and only the ones which user wants to update gets updated
+router.route("/avatar").get(verify_jwt,upload.single("avatar"),update_user_avatar)
+router.route("/cover_image").get(verify_jwt,upload.single("cover_image"),update_user_cover_image)
+router.route("/c/:username").get(verify_jwt,get_user_channel_profile)
+router.route("/history").get(verify_jwt,get_watch_history)
 export default router
